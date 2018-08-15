1f3171f21492ecc33d96248b66bc93d858f39d4b
Anstice <co674338no@gmail.com>
2018-08-15 16:35:49 +0800

    3種基本AlertDialog AlertDialog Items AlertDialog MultiChoice AlertDialog SingleChoice AlertDialog Fragment

--- app/src/main/java/com/example/android/a20180814_dialog/MainActivity.java
+++ app/src/main/java/com/example/android/a20180814_dialog/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.android.a20180814_dialog;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
-        implements DialogInterface.OnClickListener{
+        implements DialogInterface.OnClickListener,
+                    MyDialogFragment.收結果{
 
 
         private TextView tv;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity
         }
 
 
+
         // 將 onClick 事件委託給 當前的Activity(居中協調)
         public void click1(View view){
             new AlertDialog.Builder(this)
@@ -36,6 +39,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(DialogInterface dialog , int which){
             tv.setText("YES");
         }
+
 
 
         //將兩個按鈕交給同一個 listener 來處理
@@ -62,6 +66,7 @@ public class MainActivity extends AppCompatActivity
         }
 
 
+
         //採用匿名內部類別，直接實作程式碼，省去另外寫類別實作
         public void click3 (View view) {
             new AlertDialog.Builder(this)
@@ -86,4 +91,110 @@ public class MainActivity extends AppCompatActivity
                     })
                     .show();
         }
+
+
+
+        //AlertDialog Items
+        // android.R.style.Theme_Holo_Light_Dialog_NoActionBar 是風格主題(可替換)
+        public void click4 (View view){
+
+            final String[] response = getResources().getStringArray(R.array.response);
+
+            new AlertDialog.Builder(this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar)
+                    .setTitle("您覺得題目難易度如何？")
+                    .setItems(response, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            tv.setText(response[i]);
+                        }
+                    })
+                    .show();
+        }
+
+
+
+        //AlertDialog MultiChoice
+        public void click5 (View view){
+            final String[] response = getResources().getStringArray(R.array.response2);
+            final boolean[] bbb = new boolean[response.length];
+
+            new AlertDialog.Builder(this)
+                    .setTitle("你喜歡的水果")
+                    .setMultiChoiceItems(response, bbb, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                            // 可省略, boolean陣列會自動更新
+                        }
+                    })
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            StringBuilder result = new StringBuilder();
+
+                            for (int i=0; i< bbb.length; i++){
+                                if (bbb[i]){
+                                    result.append(response[i]).append("\n");
+                                }
+                            }
+                            tv.setText(result);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            tv.setText("無語");
+                        }
+                    })
+                    .show();
+
+        }
+
+
+
+        //AlertDialog SingleChoice
+        private int mChoice;    //欄位
+        public void click6 (View view){
+            final String[] response = getResources().getStringArray(R.array.response2);
+            mChoice = 0;
+            new AlertDialog.Builder(this)
+                    .setTitle("挑一種水果")
+                    .setSingleChoiceItems(response, mChoice, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            mChoice = i; //此 onClick 執行完, i 會消失, 所以要記錄在欄位裡
+                        }
+                    })
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            tv.setText(response[mChoice]);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            tv.setText("無語");
+                        }
+                    })
+                    .show();
+        }
+
+
+
+        //AlertDialog Fragment
+        // 自訂 帥氣
+        public void click7 (View view){
+            DialogFragment dialog = new MyDialogFragment();
+            dialog.show(getSupportFragmentManager(), "MyDialogFragment");
+        }
+        @Override
+        public void ok() {
+            tv.setText("登入");
+        }
+
+        @Override
+        public void cancel() {
+            tv.setText("取消登入");
+        }
+
 }
---
+++ app/src/main/java/com/example/android/a20180814_dialog/MyDialogFragment.java
@@ -0,0 +1,85 @@
+package com.example.android.a20180814_dialog;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyDialogFragment extends DialogFragment {
+
+    //合約
+    public interface 收結果{
+        public abstract void ok();
+        public abstract void cancel();
+    }
+
+    private EditText username;
+    private EditText password;
+
+    // 無參數建構子是必須的
+    public MyDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        // 透過inflater, 讀取 fragment.xml 資源來生成 View
+
+        // 取得 inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // 從 fragment.xml 取得自定畫面
+        // inflate(resource, viewGroup)
+        View view = inflater.inflate(R.layout.fragment_my_dialog, null);
+
+        //初始化 edit_view
+        username = (EditText)view.findViewById(R.id.ed_username);
+        password = (EditText)view.findViewById(R.id.ed_password);
+
+        // 建立 AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(view)
+                .setPositiveButton("Sign in", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Activity activity = getActivity();
+                        if (activity instanceof 收結果){
+                            收結果 x = (收結果) activity;
+                            x.ok();
+                        }else{
+                            throw new RuntimeException("搞什麼,Activity 沒有實作 收結果");
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Activity activity = getActivity();
+                        if (activity instanceof 收結果){
+                            收結果 x = (收結果) activity;
+                            x.cancel();
+                        }else{
+                            throw new RuntimeException("搞什麼,Activity 沒有實作 收結果");
+                        }
+                    }
+                });
+        return builder.create();
+
+    }
+}
